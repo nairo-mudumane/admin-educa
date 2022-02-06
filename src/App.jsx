4c671df02bf87905175cef29db20a5bf3388274f
nairo-mudumane <nazirmudumane@gmail.com>
2022-02-06 14:32:29 +0200

    nothing changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './assets/libs/boxicons-2.1.1/css/boxicons.min.css';
-import './scss/App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Blank from './pages/Blank';
-import Dashboard from './pages/Dashboard';
-import MainLayout from './layout/MainLayout';
+import "./assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./scss/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blank from "./pages/Blank";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
